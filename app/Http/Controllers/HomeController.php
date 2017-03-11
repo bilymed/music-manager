ccd6314d361189b9e2d34058b4eddb4cdc5fd218
Mohammed Nabil <Mohammed Nabil>
2017-03-11 18:16:26 +0000

    First commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use App\File;
+
+
+class HomeController extends Controller{
+
+    public function index(){
+        $files = File::all();
+        return view('index', ['files' => $files]);
+    }
+
+    public function upload(Request $request){
+
+        $file = $request->file('file');
+        $name = $file->getClientOriginalName();
+        $ext = $file->getClientOriginalExtension();
+
+       $path = $file->storeAs('music/', "{$name}.{$ext}");
+
+       $file = new \App\File();
+       $file->path = $path;
+       $file->save();
+
+      // $request->file('file')->store('music');
+      // return back();
+    }
+}
